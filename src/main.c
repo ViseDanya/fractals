51672919a7af2c617937574184854e4c2a618dca
Daniel Konviser <djkonviser@gmail.com>
2024-12-24 23:12:36 -0500

    julia set animation

--- src/main.c
+++ src/main.c
@@ -2,22 +2,31 @@
 
 #include "SDL3/SDL.h"
 #include <stdlib.h>
+#include <math.h>
 
-# define GL_SILENCE_DEPRECATION 1
+#define GL_SILENCE_DEPRECATION 1
 #include <OpenGL/gl3ext.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
-#define MAX_ITERATIONS 2000
+#define MAX_ITERATIONS 1000
 
-static SDL_Window* window = NULL;
+static SDL_Window *window = NULL;
 static SDL_GLContext context = NULL;
 
 static GLfloat vertices[] = {
-    -1, -1, 0,
-      1, -1, 0,
-      1,  1, 0,
-      -1,  1, 0,
+    -1,
+    -1,
+    0,
+    1,
+    -1,
+    0,
+    1,
+    1,
+    0,
+    -1,
+    1,
+    0,
 };
 
 static GLuint vertexArrayObject, vertexBufferObject;
@@ -25,28 +34,30 @@ static GLuint shaderProgram;
 
 void initSDL()
 {
-  if (!SDL_Init(SDL_INIT_VIDEO)) {
-      SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_Init failed: %s", SDL_GetError());
-      SDL_Quit();
-      exit(1);
-    }
+  if (!SDL_Init(SDL_INIT_VIDEO))
+  {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_Init failed: %s", SDL_GetError());
+    SDL_Quit();
+    exit(1);
+  }
 
-    window = SDL_CreateWindow("Fractals", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
-    if (window == NULL) {
-      SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_CreateWindow failed: %s", SDL_GetError());
-      SDL_Quit();
-      exit(1);
-    }
+  window = SDL_CreateWindow("Fractals", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+  if (window == NULL)
+  {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_CreateWindow failed: %s", SDL_GetError());
+    SDL_Quit();
+    exit(1);
+  }
 }
 
 void initOpenGL()
 {
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-  SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
   context = SDL_GL_CreateContext(window);
-  if( context == NULL )
+  if (context == NULL)
   {
     SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_GL_CreateContext failed: %s", SDL_GetError());
     SDL_Quit();
@@ -60,7 +71,7 @@ void initOpenGL()
   glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
   glEnableVertexAttribArray(0);
 
   shaderProgram = glCreateProgram();
@@ -74,9 +85,10 @@ void cleanup()
   SDL_Quit();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   initSDL();
-  initOpenGL();    
+  initOpenGL();
 
   bool quit = false;
   SDL_Event event;
@@ -85,35 +97,55 @@ int main(int argc, char *argv[]) {
   float prevMouseX, prevMouseY = 0;
   float deltaX, deltaY = 0;
   float panX, panY = 0;
-  while(!quit)
+
+  float juliaX = 0;
+  float juliaY = 0;
+  bool pause = false;
+  Uint32 prevTime = SDL_GetTicks();
+  Uint32 currentTime = SDL_GetTicks();
+  while (!quit)
   {
-    while( SDL_PollEvent(&event) != 0 )
+    while (SDL_PollEvent(&event) != 0)
     {
-        if( event.type == SDL_EVENT_QUIT )
-        {
-          quit = true;
-        }
-        else if (event.type == SDL_EVENT_MOUSE_WHEEL)
-        {
-          zoom *= (event.wheel.y > 0) ? 1.1f : 0.9f; // Zoom in if scrolling up, zoom out if scrolling down
-        }
+      if (event.type == SDL_EVENT_QUIT)
+      {
+        quit = true;
+      }
+      else if (event.type == SDL_EVENT_MOUSE_WHEEL)
+      {
+        zoom *= (event.wheel.y > 0) ? 1.1f : 0.9f; // Zoom in if scrolling up, zoom out if scrolling down
+      }
+      else if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_SPACE)
+      {
+        pause = !pause;
+      }
     }
 
     prevMouseX = mouseX;
     prevMouseY = mouseY;
-   if(SDL_GetMouseState(&mouseX, &mouseY) & SDL_BUTTON_LMASK)
-   {
+    if (SDL_GetMouseState(&mouseX, &mouseY) & SDL_BUTTON_LMASK)
+    {
       deltaX = mouseX - prevMouseX;
       deltaY = mouseY - prevMouseY;
-      panX -= deltaX/(WINDOW_WIDTH * zoom);
-      panY += deltaY/(WINDOW_HEIGHT * zoom);
-   }
+      panX -= deltaX / (WINDOW_WIDTH * zoom);
+      panY += deltaY / (WINDOW_HEIGHT * zoom);
+    }
 
-    glClearColor( 0.f, 0.f, 0.f, 1.f );
+    if (!pause)
+    {
+      Uint32 timeDiff = SDL_GetTicks() - prevTime;
+      currentTime += timeDiff;
+      juliaX = -.77;
+      juliaY = (sin((float)(currentTime) / 1000) + 1) / 2 * .5 + -.25;
+    }
+    prevTime = SDL_GetTicks();
+
+    // printf("%f\n", juliaY);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(shaderProgram);
-    GLint maxIterationsLocation = glGetUniformLocation(shaderProgram, "maxIterations");
-    glUniform1i(maxIterationsLocation, MAX_ITERATIONS);
+    GLint juliaLocation = glGetUniformLocation(shaderProgram, "julia");
+    glUniform2f(juliaLocation, juliaX, juliaY);
     GLint zoomLocation = glGetUniformLocation(shaderProgram, "zoom");
     glUniform1f(zoomLocation, zoom);
     GLint panLocation = glGetUniformLocation(shaderProgram, "pan");
@@ -123,6 +155,6 @@ int main(int argc, char *argv[]) {
 
     SDL_GL_SwapWindow(window);
   }
-  
+
   cleanup();
 }
--- src/shaders/fragShader.glsl
+++ src/shaders/fragShader.glsl
@@ -2,27 +2,75 @@
 in vec2 pos;
 out vec4 fragColor;
 
-uniform int maxIterations;
-void main()
+const int maxIterations = 1000;
+float xs[maxIterations];
+float ys[maxIterations];
+
+uniform vec2 julia;
+
+int getDivergenceIteration(float x0, float y0)
 {
-  float x,x0 = pos.x;
-  float y,y0 = pos.y;
+  float x = x0;
+  float y = y0;
   int iteration = 0;
   while (x*x + y*y <= 2*2 && iteration < maxIterations)
   {
-        float xtemp = x*x - y*y + x0;
-        y = 2*x*y + y0;
+        xs[iteration] = x;
+        ys[iteration] = y;
+        float xtemp = x*x - y*y + julia.x;//+ x0;
+        y = 2*x*y + julia.y;//y0;
         x = xtemp;
         ++iteration;
   }
+  return iteration;
+}
 
-  if(iteration != maxIterations)
-  {
-    fragColor = vec4(float(iteration)/float(100),0.0,0.0,1.0);
-  }
-  else
-  {
-    fragColor = vec4(0.0,0.0,0.0,1.0);
-  }
+bool convergesToCycleWithPeriod(int period)
+{
+  int convergenceIteration = 0;
+  while(convergenceIteration + period < maxIterations)
+    {
+        float prevx = xs[convergenceIteration];
+        float prevy = ys[convergenceIteration];
+        float x = xs[convergenceIteration + period];
+        float y = ys[convergenceIteration + period];
+        float xDiff = x-prevx;
+        float yDiff = y-prevy;
+        if(xDiff*xDiff + yDiff*yDiff < 10e-6)
+        {
+            return true;
+        }
+        convergenceIteration++;
+    }
+    return false;
+}
+void main()
+{
+  int divergenceIteration = getDivergenceIteration(pos.x, pos.y);
 
+  // if(divergenceIteration == maxIterations)
+  // {
+  //   if(convergesToCycleWithPeriod(1))
+  //   {
+  //     fragColor = vec4(1.0,0.0,0.0,1.0);
+  //   }
+  //   else if(convergesToCycleWithPeriod(2))
+  //   {
+  //     fragColor = vec4(0.0,1.0,0.0,1.0);
+  //   }
+  // }
+  // else
+  // {
+    fragColor = vec4(mod(divergenceIteration,2));
+  //}
+
+  // if(iteration != maxIterations)
+  // {
+  //   float intensity = mod(float(iteration),10)/10;
+  //   fragColor = vec4(intensity,intensity,intensity,1.0);
+  // }
+  // else
+  // {
+  //   fragColor = vec4(0.0,0.0,0.0,1.0);
+  // }
 }
